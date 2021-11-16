faaf66f8f22d0f0c549cec254aec8c86da260214
Slisk Lindqvist| Querzion <39269026+Querzion@users.noreply.github.com>
2021-11-16 03:10:17 +0000

    Delete Virtual Machines/QEMU-KVM directory

--- Virtual Machines/QEMU-KVM/etc/libvirt/hooks/qemu.d/win10/prepare/begin/isolstart.sh
+++
@@ -1,5 +0,0 @@
-# CPU PINNING	-	lscpu -e # Lock linux to only using 1 core & 1 logic core
-
-systemctl set-property --runtime -- user.slice AllowedCPUs=0,8
-systemctl set-property --runtime -- system.slice AllowedCPUs=0,8
-systemctl set-property --runtime -- init.scope AllowedCPUs=0,8
--- Virtual Machines/QEMU-KVM/etc/libvirt/hooks/qemu.d/win10/release/end/isocpurevert.sh
+++
@@ -1,5 +0,0 @@
-# CPU PINNING   -       lscpu -e # Unlock all cores for GNU/Linux
-
-systemctl set-property --runtime -- user.slice AllowedCPUs=0-15u
-systemctl set-property --runtime -- system.slice AllowedCPUs=0-15
-systemctl set-property --runtime -- init.scope AllowedCPUs=0-15
